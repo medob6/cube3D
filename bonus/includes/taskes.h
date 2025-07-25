da87c5856e590e839d20ea78a0cc92fa8756a744
Mohamed Boussetta <mbousset@c3r10p4.1337.ma>
2025-07-25 15:40:50 +0100

    playing with vid

---
+++ bonus/includes/taskes.h
@@ -0,0 +1,23 @@
+# ifndef TASKES_H
+#define TASKES_H
+
+
+typedef struct  s_task
+{
+    /* data */
+} t_task;
+
+typedef struct s_task_queue
+{
+    /* data */
+}t_task_queue;
+
+
+// creat a task
+
+// we have a queue of task that all the workers e.g theads can submit (add to the queue) , excute (remove fro queue) 
+
+
+
+
+#endif
